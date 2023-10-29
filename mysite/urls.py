17358171b00f7db613e5a7f2df470bf3b992ea2e
JJoÃão Paulo <jp.sport17@gmail.com>
2023-10-29 01:00:44 -0300

    feat: Mapeando URL do index de Voluntario e ONG

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('solidify', include("solidify.urls")),
+    path('ong/', include("ong.urls")),
+    path('voluntario/', include("voluntario.urls")),
     path('admin/', admin.site.urls),
 ]
