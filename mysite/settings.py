489bc5e8a47f3ea5a6616a425a2ab9bc7132e3c5
JJoÃão Paulo <jp.sport17@gmail.com>
2024-01-17 09:01:50 -0300

    bug: ajuste nos dados de conexão das variáveis de ambiente

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,8 +16,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATABASE_URL = os.environ.get('DATABASE_URL')
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -83,7 +81,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=DATABASE_URL,
+        os.environ.get('DATABASE_URL'),
         conn_max_age=600
     )
 }
